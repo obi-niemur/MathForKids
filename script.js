fc737dfb5f199b3f41074407fa2f468e9ed35c7a
Niemur Obi <niemur.rahman30@gmail.com>
2022-01-09 19:21:57 -0500

    Condition correct or wrong working

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
-
 // Dom calling all the top nav math option
 const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
 const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 
+let result; // result is global
 
 // DOM calling all the option
 const opt_sec = document.getElementById("option_sec");
@@ -18,60 +16,48 @@ const opt3 = document.getElementById("option3");
 const a1 = document.getElementById("add1");
 
 //Adding event listener to options
-opt1.addEventListener("click", function(event){
-  if(event.click === this.result){
-    console.log('correct ans');
+opt1.addEventListener("click", function () {
+  if (opt1.innerHTML == result) {
+    console.log("correct ans");
     addingNum();
-  }else {
-    console.log('not true')
+  } else {
+    console.log("not true");
   }
-    
-  // return event
-
 });
-// opt1.addEventListener('click', addingNum);
 
-
-opt2.addEventListener("click", function(event){
-  if(event.click === this.result){
-    console.log('correct ans');
+opt2.addEventListener("click", function () {
+  if (opt2.innerHTML == result) {
+    console.log("correct ans");
     addingNum();
-  }else {
-
-    console.log('not true')
-
+  } else {
+    console.log("not true");
   }
+
   
-    
-  // return event
-
 });
 
-// opt2.addEventListener("click", addingNum);
 
-opt3.addEventListener("click", function(event){
-  if(event.click === this.result){
-    console.log('correct ans');
+
+opt3.addEventListener("click", function () {
+  if (opt3.innerHTML == result) {
+    console.log("correct ans");
     addingNum();
-  }else  {
-    console.log('not true')
+  } else {
+    console.log("not true");
   }
-    
-  // return event
 
 });
 
-// opt3.addEventListener("click", addingNum);
+
 
 //Whenever user choose add it will call adding num function
 add.addEventListener("click", addingNum);
 
 function addingNum() {
   let num1; //this will hold random number 1
-  let num2;  //this will  hold random number 2
-  let result;  //this will hold result
+  let num2; //this will  hold random number 2
+  //this will hold result
   var switchAnswers = []; //empty array for random option
-
 
   // this for loop will decide to choose number from 1 to 100
   for (let i = 0; i <= 100; i++) {
@@ -86,18 +72,14 @@ function addingNum() {
   var allAns = [opt, opt2_1, result];
 
   // This for loop will add the options from allans array to switch ans
-  for (i = allAns.length; i--;){
-    switchAnswers.push(allAns.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
-  };
-
-  
-
+  for (i = allAns.length; i--; ) {
+    switchAnswers.push(
+      allAns.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
+    );
+  }
 
   a1.innerHTML = num1 + " + " + num2 + " = " + result;
   opt1.innerHTML = switchAnswers[0];
   opt2.innerHTML = switchAnswers[1];
   opt3.innerHTML = switchAnswers[2];
 }
-
-  
-
